5a9b6392325bc40b3cd2cd1d5711a059168c00dc
Cason Adams <cadams@roku.com>
2022-04-11 09:30:54 -0600

    first commit

--- profile_helper.sh
+++ profile_helper.sh
@@ -5,6 +5,14 @@ os="$(uname -s)"
 _ALACRITTY_YML="${HOME}/.config/alacritty/alacritty.yml"
 echo "export _ALACRITTY_YML=${_ALACRITTY_YML}"
 
+_YQ_PATH="${K9S_THEME_GEN}/yq"
+yq_in_path="$(command -v yq)"
+if [ -x "${yq_in_path}" ]; then
+  _YQ_PATH="$(which yq)"
+fi
+
+echo "export _YQ_PATH=${_YQ_PATH}"
+
 if [ -n "${XDG_CONFIG_HOME+x}" ]; then
   _K9s_CONFIG="${XDG_CONFIG_HOME}/k9s"
 elif [ "${os}" == "Darwin" ]; then
@@ -21,7 +29,9 @@ _k9s() {
   local yq background foreground red green yellow blue magenta cyan brightblack brightwhite orange black
   mkdir -p "${_K9s_CONFIG}"
   cp "${K9S_THEME_GEN}/skin.yml" "${_K9s_CONFIG}/"
-  yq="${K9S_THEME_GEN}/yq"
+  yq="${_YQ_PATH}"
+
+
   "$yq" -i eval-all "select(fileIndex==0).background  = select(fileIndex==1).schemes.$theme.primary.background      | select(fileIndex==0)" "${_K9s_CONFIG}/skin.yml" "${_ALACRITTY_YML}"
   "$yq" -i eval-all "select(fileIndex==0).foreground  = select(fileIndex==1).schemes.$theme.primary.foreground      | select(fileIndex==0)" "${_K9s_CONFIG}/skin.yml" "${_ALACRITTY_YML}"
   "$yq" -i eval-all "select(fileIndex==0).red         = select(fileIndex==1).schemes.$theme.normal.red              | select(fileIndex==0)" "${_K9s_CONFIG}/skin.yml" "${_ALACRITTY_YML}"
